75cbf2c533a2ba6f9d158399981199790f601818
Patrick Owens <patrick@fractalfox.com>
2016-03-10 20:35:55 -0800

    fixes

--- Achievements/Assets/ReportAchievementOnClick.cs
+++ Achievements/Assets/ReportAchievementOnClick.cs
@@ -6,6 +6,31 @@ public class ReportAchievementOnClick : MonoBehaviour
 
     void OnMouseDown()
     {
-        // Report progress percentage toward achievement
-    }
+		Prime31.GameCenterManager.reportAchievementFailedEvent += reportAchievementFailed;
+		Prime31.GameCenterManager.reportAchievementFinishedEvent += reportAchievementFinished;
+		// Report progress percentage toward achievement
+		if (Prime31.GameCenterBinding.isPlayerAuthenticated()) 
+		{
+			Prime31.GameCenterBinding.reportAchievement (Identifier, 100.0f);
+			Prime31.GameCenterBinding.showCompletionBannerForAchievements ();
+		}
+
+		else
+		{
+			Debug.Log ("Player was not authenticated shuga");
+		}
+	
+	}
+
+
+	void reportAchievementFinished( string identifier )
+	{
+		Debug.Log( "reportAchievementFinishedEvent: " + identifier );
+	}
+
+		
+	void reportAchievementFailed( string error )
+	{
+		Debug.Log( "reportAchievementFailedEvent: " + error );
+	}
 }
